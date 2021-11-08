6b2f0f02bf4bcfe08e798b27190d89edfa75297b
prettywang2002 <shelly.tycg@gmail.com>
2021-11-08 13:53:53 +0800

    git init

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+  <Card></Card>
+</template>
+
+<script>
+import Card from '@/components/Card.vue'
+
+// 要把卡片js匯出所以要export...
+export default {
+  components: {
+    Card
+  }
+}
+</script>
